9605231a281d7cfaf2e6761bf5682266b5275c90
Jeff <Jeff.Peerson@AlaskaAir.com>
2017-02-27 11:43:27 -0800

    Homework1

---
+++ HelloWorld/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+// Jeff Peerson   D220  Client Applications   Homework 1
+
+namespace HelloWorld
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()   //constructor
+        {
+            InitializeComponent();
+        }
+
+        private void uxSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            string sMsg = "Submitting password: " + uxPassword.Text;
+            MessageBox.Show(sMsg);
+        }
+
+        private void uxName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            EnableSubmitButton();
+        }
+
+        private void uxPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            EnableSubmitButton();
+        }
+
+        private void EnableSubmitButton()
+        {
+            if (uxName.Text.Length > 0 && uxPassword.Text.Length > 0)
+            {
+                uxSubmit.IsEnabled = true;
+            }
+            else
+            {
+                uxSubmit.IsEnabled = false;
+            }
+        }
+    }
+}
